f82e63e6c91bdcdd9a0281f59becde806e42c97d
Anne <anais.kare@gmail.com>
2018-02-13 17:41:47 -0300

    Mejoras item Cotizacion 13-02-2018
    Se realizan pequeñas mejoras en las view models en cotizacion

--- Sertec.View/Models/DashboardViewModels.cs
+++ Sertec.View/Models/DashboardViewModels.cs
@@ -7,12 +7,26 @@ namespace Sertec.View.Models
 {
     public class CotizacionViewModel
     {
+        //primera parte 
         public int PresupuestoId { get; set; }
+        public DateTime FechaEmision { get; set; }
+        public string Ascensor { get; set; }
+        public string TecEmisor { get; set; }
+        public string Supervisor { get; set; }
+        public string PresupuestoNumero { get; set; }
+        public string ValorUf { get; set; }
+        public string ValorHP { get; set; }
+        public int ValorFlete { get; set; }
+        public string DetalleDescrip { get; set; }
+
+
+
+        //Ultima parte
+        public int DuracionTrabajo { get; set; }
         public string Descripcion { get; set; }
         public int ValorMoneda { get; set; }
         public int ValorHH { get; set; }
-        public int ValorFlete { get; set; }
-        public DateTime FechaEmision { get; set; }
+
     }
 
     public class PresupuestoRepuestoViewModel
@@ -24,5 +38,12 @@ namespace Sertec.View.Models
         public int ValorUnitario { get; set; }
         public int SubTotal { get; set; }
         public int HoraParHombre { get; set; }
+    }
+    public class PresupuestoTrabajoTercerosViewModel
+    {
+     
+        public string Terceros { get; set; } 
+        public int ValTer { get; set; }
+  
     }
 }
